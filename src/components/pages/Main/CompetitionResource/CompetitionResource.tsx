d31a0ebb1c9369605d3d6e4f48ce05f921191df1
Denwa <denwa799@yandex.ru>
2022-01-18 12:49:09 +0300

    Add competition resource calendar page and connect redux dev tools

--- src/components/pages/Main/CompetitionResource/CompetitionResource.tsx
+++ src/components/pages/Main/CompetitionResource/CompetitionResource.tsx
@@ -1,10 +1,10 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './CompetitionResource.module.css';
 import { useTypedSelector } from '../../../../hooks/useTypedSelector';
 import { useDispatch } from 'react-redux';
 import { fetchCompetition } from '../../../../store/action-creators/CompetitionResource';
 import { useParams } from 'react-router-dom';
-import { Typography } from 'antd';
+import { Typography, Table } from 'antd';
 import Container from '../../../UI/Container/Container';
 
 const { Title } = Typography;
@@ -16,13 +16,30 @@ interface CompetitionResourceParams {
 const CompetitionResource: React.FC = () => {
   const { competition, error, loading } = useTypedSelector((state) => state.competitionResource);
   const dispatch = useDispatch();
+  const [dataSource, setDataSource] = useState<any[]>([]);
 
   // @ts-ignore
   const params = useParams<CompetitionResourceParams>();
+
   useEffect(() => {
     // @ts-ignore
     dispatch(fetchCompetition(params.id));
   }, []);
+
+  useEffect(() => {
+    if (Object.keys(competition).length != 0) {
+      const matches = competition.matches.map((match: any) => {
+        return {
+          status: match.status,
+          date: `${new Date(match.utcDate).toLocaleString()} `,
+          homeTeam: match.homeTeam.name,
+          awayTeam: match.awayTeam.name,
+          score: `${match.score.fullTime.homeTeam} : ${match.score.fullTime.awayTeam}`,
+        };
+      });
+      setDataSource(matches);
+    }
+  }, [competition]);
 
   if (loading) {
     return <h1 className={styles.loading}>Идет загрузка...</h1>;
@@ -32,13 +49,42 @@ const CompetitionResource: React.FC = () => {
     return <h1 className={styles.loading}>{error}</h1>;
   }
 
-  console.log(competition);
+  const columns = [
+    {
+      title: 'Статус',
+      dataIndex: 'status',
+      key: 'status',
+    },
+    {
+      title: 'Дата и время',
+      dataIndex: 'date',
+      key: 'date',
+    },
+    {
+      title: 'Домашняя команда',
+      dataIndex: `homeTeam`,
+      key: 'homeTeam',
+    },
+    {
+      title: 'Команда противника',
+      dataIndex: `awayTeam`,
+      key: 'awayTeam',
+    },
+    {
+      title: 'Счет',
+      dataIndex: `score`,
+      key: 'score',
+    },
+  ];
 
   return (
     <div className={styles.CompetitionResource}>
       <Container>
         {Object.keys(competition).length != 0 ? (
-          <Title className={styles.Title}>{competition.competition.name}</Title>
+          <div>
+            <Title className={styles.Title}>{competition.competition.name}</Title>
+            <Table columns={columns} dataSource={dataSource} pagination={false} />
+          </div>
         ) : (
           <h1>Соревнование не найдено</h1>
         )}
