50e2f3a204f5e4207f63db150ebb0ce01e5bdc7b
Denwa <denwa799@yandex.ru>
2022-01-19 17:15:14 +0300

    Add search input to competitions page

--- src/components/pages/Main/CompetitionList/CompetitionList.tsx
+++ src/components/pages/Main/CompetitionList/CompetitionList.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './CompetitionList.module.css';
 import { useTypedSelector } from '../../../../hooks/useTypedSelector';
 import { Card, Col, Row } from 'antd';
@@ -6,12 +6,20 @@ import noImage from '../../../../assets/img/noImage.jpg';
 import { useDispatch } from 'react-redux';
 import { fetchCompetitions } from '../../../../store/action-creators/CompetitionList';
 import { Link } from 'react-router-dom';
+import { SearchOutlined } from '@ant-design/icons';
 
 const { Meta } = Card;
 
 const CompetitionList: React.FC = () => {
   const { competitions, error, loading } = useTypedSelector((state) => state.competitionList);
   const dispatch = useDispatch();
+
+  const [value, setValue] = useState('');
+  const [isOpen, setIsOpen] = useState(true);
+
+  const filtredCompetitions = competitions.filter((competition) => {
+    return competition.name.toLowerCase().includes(value.toLocaleLowerCase());
+  });
 
   useEffect(() => {
     dispatch(fetchCompetitions());
@@ -25,11 +33,48 @@ const CompetitionList: React.FC = () => {
     return <h1>{error}</h1>;
   }
 
+  const itemClickHandler = (e: any) => {
+    setValue(e.target.textContent);
+    setIsOpen(!isOpen);
+  };
+
+  const inputClickHandler = () => {
+    setIsOpen(true);
+  };
+
   return (
     <div className={styles.CompetitionList}>
+      <Row>
+        <Col span={7}>
+          <form className={styles.searchForm}>
+            <input
+              type="text"
+              placeholder="Введите название соревнования..."
+              className={styles.searchInput}
+              value={value}
+              onChange={(event) => setValue(event.target.value)}
+              onClick={inputClickHandler}
+            />
+            <ul className={styles.autocomplete}>
+              {competitions.length != 0 && value && isOpen
+                ? filtredCompetitions.map((competition) => (
+                    <li
+                      key={competition.id}
+                      className={styles.autocompleteItem}
+                      onClick={itemClickHandler}
+                    >
+                      {competition.name}
+                    </li>
+                  ))
+                : null}
+            </ul>
+            <SearchOutlined className={styles.searchIcn} />
+          </form>
+        </Col>
+      </Row>
       <Row className={styles.Cards} gutter={[16, 16]}>
         {competitions.length != 0 ? (
-          competitions.map((competition) => (
+          filtredCompetitions.map((competition) => (
             <Col key={competition.id} span={8}>
               <Link to={`${competition.id}`}>
                 <Card
