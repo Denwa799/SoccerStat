7d65879e78df8e2c304e10d1477e10deea08c429
Denwa <denwa799@yandex.ru>
2022-02-14 12:51:25 +0300

    Fixed eslint ignore in app.js second attempt

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-import path from 'path';
-import express from 'express';
-
+const express = require('express');
 const app = express();
+
+const path = require('path');
+
 const port = process.env.PORT || 5000;
 
 if (process.env.NODE_ENV === 'production') {
