df08339a8377d423054dded41027cfa5aa13dd59
Denwa <denwa799@yandex.ru>
2022-02-26 16:52:00 +0300

    Add lazy loading

--- src/components/AppRouter/AppRouter.tsx
+++ src/components/AppRouter/AppRouter.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import {
   MAIN_ROUTE,
@@ -8,22 +8,72 @@ import {
   TEAM_ID_ROUTE,
   MATCH_ID_ROUTE,
 } from '../../utils/routes';
-import CompetitionResource from '../pages/Main/CompetitionResource/CompetitionResource';
-import Main from '../pages/Main/Main';
-import Matches from '../pages/Matches/Matches';
-import MatchResource from '../pages/Matches/MatchResource/MatchResource';
-import TeamResource from '../pages/Teams/TeamResource/TeamResource';
-import Teams from '../pages/Teams/Teams';
+import { Spin } from 'antd';
+import { LoadingOutlined } from '@ant-design/icons';
+import styles from '../UI/ErrorLoading/ErrorLoading.module.css';
+
+const Main = React.lazy(() => import('../pages/Main/Main'));
+const Matches = React.lazy(() => import('../pages/Matches/Matches'));
+const Teams = React.lazy(() => import('../pages/Teams/Teams'));
+const CompetitionResource = React.lazy(
+  () => import('../pages/Main/CompetitionResource/CompetitionResource')
+);
+const MatchResource = React.lazy(() => import('../pages/Matches/MatchResource/MatchResource'));
+const TeamResource = React.lazy(() => import('../pages/Teams/TeamResource/TeamResource'));
+
+const antIcon = <LoadingOutlined className={styles.Loader} spin />;
 
 const AppRouter: React.FC = () => {
   return (
     <Routes>
-      <Route path={MAIN_ROUTE} element={<Main />} />
-      <Route path={MATCHES_ROUTE} element={<Matches />} />
-      <Route path={TEAMS_ROUTE} element={<Teams />} />
-      <Route path={COMPETITION_ID_ROUTE} element={<CompetitionResource />} />
-      <Route path={MATCH_ID_ROUTE} element={<MatchResource />} />
-      <Route path={TEAM_ID_ROUTE} element={<TeamResource />} />
+      <Route
+        path={MAIN_ROUTE}
+        element={
+          <Suspense fallback={<Spin indicator={antIcon} />}>
+            <Main />
+          </Suspense>
+        }
+      />
+      <Route
+        path={MATCHES_ROUTE}
+        element={
+          <Suspense fallback={<Spin indicator={antIcon} />}>
+            <Matches />
+          </Suspense>
+        }
+      />
+      <Route
+        path={TEAMS_ROUTE}
+        element={
+          <Suspense fallback={<Spin indicator={antIcon} />}>
+            <Teams />
+          </Suspense>
+        }
+      />
+      <Route
+        path={COMPETITION_ID_ROUTE}
+        element={
+          <Suspense fallback={<Spin indicator={antIcon} />}>
+            <CompetitionResource />
+          </Suspense>
+        }
+      />
+      <Route
+        path={MATCH_ID_ROUTE}
+        element={
+          <Suspense fallback={<Spin indicator={antIcon} />}>
+            <MatchResource />
+          </Suspense>
+        }
+      />
+      <Route
+        path={TEAM_ID_ROUTE}
+        element={
+          <Suspense fallback={<Spin indicator={antIcon} />}>
+            <TeamResource />
+          </Suspense>
+        }
+      />
       <Route path="*" element={<Navigate replace to="/" />} />
     </Routes>
   );
