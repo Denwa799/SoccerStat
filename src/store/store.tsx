0aa28f81a2581de72f2120d65b50ab96f668479a
Denwa <denwa799@yandex.ru>
2022-02-28 21:19:01 +0300

    Add redux devtools type

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -2,6 +2,11 @@ import { applyMiddleware, compose, createStore } from 'redux';
 import thunk from 'redux-thunk';
 import { rootReducer } from './reducers';
 
-// @ts-ignore
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+  }
+}
+
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 export const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
